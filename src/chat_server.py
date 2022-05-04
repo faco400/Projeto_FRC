b605af4bc3f4b8095c7e182adfc1e109fc1ea832
RuanMoura <ruanmoura13@outlook.com>
2022-05-03 21:11:27 -0300

    add quit command

--- src/chat_server.py
+++ src/chat_server.py
@@ -14,8 +14,8 @@ a continuous flow."""
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-host = "127.0.0.1" # servidor vai rodar na maquina local
-Port = 8080 # porta
+host = "127.0.0.1"  # servidor vai rodar na maquina local
+Port = 8080  # porta
 
 """
 binds the server to an entered IP address and at the
@@ -30,116 +30,133 @@ increased as per convenience.
 """
 server.listen(100)
 
-list_of_clients = [] # armazena a lista de clientes
-list_of_nicknames = [] # armazena a lista de apelidos dos clientes
+list_of_clients = []  # armazena a lista de clientes
+list_of_nicknames = []  # armazena a lista de apelidos dos clientes
+list_of_rooms = []
 
 """Metodo que ira tratar conexoes com os clientes
 sempre buscando receber uma mensagem e fazendo o broadcast
 para os demais clientes"""
-def handle(conn):
-	while True:
-			# try:
-				msg = message = conn.recv(2048)
-				if msg.decode('ascii').startswith('KICK'):
-					if list_of_nicknames[list_of_clients.index(conn)] == 'admin':
-						name_to_kick = msg.decode('ascii')[5:]
-						kick_user(name_to_kick)
-					else:
-						conn.send('Comando foi recusado!'.encode('ascii'))
-				
-				elif msg.decode('ascii').startswith('BAN'):
-					if list_of_nicknames[list_of_clients.index(conn)] == 'admin':
-						name_to_ban = msg.decode('ascii')[4:]
-						kick_user(name_to_ban)
-						with open('bans.txt', 'a') as f:
-							f.write(f'{name_to_ban}\n')
-						print(f'{name_to_ban} foi banido!')
-					else:
-						conn.send('Comando foi recusado!'.encode('ascii'))
 
-				else:
-					broadcast(message)
-			# except:
-				# print('aiaiai')
-				# remove(conn)
-				# break
+
+def handle(conn):
+    while True:
+        # try:
+        msg = message = conn.recv(2048)
+        if msg.decode('ascii').startswith('KICK'):
+            if list_of_nicknames[list_of_clients.index(conn)] == 'admin':
+                name_to_kick = msg.decode('ascii')[5:]
+                kick_user(name_to_kick)
+            else:
+                conn.send('Comando foi recusado!'.encode('ascii'))
+        elif msg.decode('ascii').startswith('BAN'):
+            if list_of_nicknames[list_of_clients.index(conn)] == 'admin':
+                name_to_ban = msg.decode('ascii')[4:]
+                kick_user(name_to_ban)
+                with open('bans.txt', 'a') as f:
+                    f.write(f'{name_to_ban}\n')
+                print(f'{name_to_ban} foi banido!')
+            else:
+                conn.send('Comando foi recusado!'.encode('ascii'))
+        elif msg.decode('ascii').startswith('QUIT'):
+            conn.send('QUIT'.encode('ascii'))
+            remove(conn)
+            return
+        else:
+            broadcast(message)
+            # except:
+        # print('aiaiai')
+        # remove(conn)
+        # break
+
 
 """Using the below function, we broadcast the message to all
 clients who's object is not the same as the one sending
 the message"""
+
+
 def broadcast(message):
-	for client in list_of_clients:
-		client.send(message)
+    for client in list_of_clients:
+        client.send(message)
+
 
 """The following function simply removes the object
 from the list that was created at the beginning of
 the program"""
+
+
 def remove(connection):
-	index = list_of_clients.index(connection)
-	list_of_clients.remove(connection)
-	connection.close()
-	nickname = list_of_nicknames[index]
-	broadcast(f'{nickname} saiu do chat'.encode('ascii'))
-	list_of_nicknames.remove(nickname)
+    index = list_of_clients.index(connection)
+    list_of_clients.remove(connection)
+    print(connection.getpeername(), "disconnected")
+    connection.close()
+    nickname = list_of_nicknames[index]
+    list_of_nicknames.remove(nickname)
+    broadcast(f'{nickname} saiu do chat'.encode('ascii'))
 
 
 def receive():
-	while True:
+    while True:
 
-		"""Accepts a connection request and stores two parameters,
-		conn which is a socket object for that user, and addr
-		which contains the IP address of the client that just
-		connected"""
-		conn, addr = server.accept()
+        """Accepts a connection request and stores two parameters,
+        conn which is a socket object for that user, and addr
+        which contains the IP address of the client that just
+        connected"""
+        conn, addr = server.accept()
 
-		# # prints the address of the user that just connected
-		print (f"{str(addr)} connected")
+        # # prints the address of the user that just connected
+        print(f"{str(addr)} connected")
 
-		#Envia uma palavra chave para o cliente escolher apelido
-		conn.send('NICK'.encode('ascii'))
-		nickname = conn.recv(2048).decode('ascii')
+        # Envia uma palavra chave para o cliente escolher apelido
+        conn.send('NICK'.encode('ascii'))
+        nickname = conn.recv(2048).decode('ascii')
 
-		with open('bans.txt','r') as f:
-			bans = f.readlines()
-		
-		if nickname+'\n' in bans:
-			conn.send('BAN'.encode('ascii'))
-			conn.close()
-			continue
+        with open('bans.txt', 'r') as f:
+            bans = f.readlines()
 
-		if nickname == 'admin':
-			conn.send('PASS'.encode('ascii'))
-			password = conn.recv(2048).decode('ascii')
+        if nickname+'\n' in bans:
+            conn.send('BAN'.encode('ascii'))
+            conn.close()
+            continue
 
-			if password != 'admin':
-				conn.send('RECUSADO'.encode('ascii'))
-				conn.close()
-				continue
+        if nickname == 'admin':
+            conn.send('PASS'.encode('ascii'))
+            password = conn.recv(2048).decode('ascii')
 
-		"""Maintains a list of clients and nicknames for ease of broadcasting
+            if password != 'admin':
+                conn.send('RECUSADO'.encode('ascii'))
+                conn.close()
+                continue
+
+        """Maintains a list of clients and nicknames for ease of broadcasting
 		a message to all available people in the chatroom"""
-		list_of_clients.append(conn)
-		list_of_nicknames.append(nickname)
+        list_of_clients.append(conn)
+        list_of_nicknames.append(nickname)
 
-		print(f'apelido do cliente e {nickname}') # Mostra no server o apelido do cliente
-		broadcast(f'{nickname} entrou no chat'.encode('ascii')) # faz um broadcast a todos sobre o cliente
-		
-		# sends a message to the client whose user object is conn
-		conn.send(f'\nBem vindo ao chat {nickname}!'.encode('ascii'))
+        # Mostra no server o apelido do cliente
+        print(f'apelido do cliente e {nickname}')
+        # faz um broadcast a todos sobre o cliente
+        broadcast(f'{nickname} entrou no chat'.encode('ascii'))
 
-		thread = threading.Thread(target=handle, args=(conn,)) # cria uma thread que ira tratar o cliente
-		thread.start() #inicia a thread
+        # sends a message to the client whose user object is conn
+        conn.send(f'\nBem vindo ao chat {nickname}!'.encode('ascii'))
+
+        # cria uma thread que ira tratar o cliente
+        thread = threading.Thread(target=handle, args=(conn,))
+        thread.start()  # inicia a thread
+
 
 def kick_user(name):
-	if name in list_of_nicknames:
-		name_index = list_of_nicknames.index(name)
-		client_to_kick = list_of_clients[name_index]
-		list_of_clients.remove(client_to_kick)
-		client_to_kick.send('Voce foi expulso da sala de bate papo'.encode('ascii'))
-		client_to_kick.close()
-		list_of_nicknames.remove(name)
-		broadcast(f'{name} foi expulso pelo admin'.encode('ascii'))
+    if name in list_of_nicknames:
+        name_index = list_of_nicknames.index(name)
+        client_to_kick = list_of_clients[name_index]
+        list_of_clients.remove(client_to_kick)
+        client_to_kick.send(
+            'Voce foi expulso da sala de bate papo'.encode('ascii'))
+        client_to_kick.close()
+        list_of_nicknames.remove(name)
+        broadcast(f'{name} foi expulso pelo admin'.encode('ascii'))
+
 
 print('Escutando servidor')
 receive()
-
--- src/client.py
+++ src/client.py
@@ -18,7 +18,8 @@ server.connect((IP_address, Port))
 
 nickname = input("escolha um apelido: ")
 if nickname == 'admin':
-  password = input('Digite a senha para o admin: ')
+    password = input('Digite a senha para o admin: ')
+room = input("Escolha uma sala: ")
 
 stop_thread = False
 
@@ -26,50 +27,72 @@ stop_thread = False
 identificação do recebimento da plavra chave NICK que permite
 o envio do apelido para o servidor. Se ocorrer algum erro é
 fechado a conexao"""
+
+
 def receive():
-  while True:
-    global stop_thread
-    if stop_thread:
-      break
-    try: 
-      message = server.recv(2048).decode('ascii')
-      if message ==  'NICK':
-        server.send(nickname.encode('ascii'))
-        next_message = server.recv(2048).decode('ascii')
-        if next_message == 'PASS':
-          server.send(password.encode('ascii'))
-          if server.recv(2048).decode('ascii') == 'REFUSE':
-            print("Conexao foi recusada. Senha errada")
-            stop_thread = True
-        elif next_message == 'BAN':
-          print("Conexao recusada por causa de ban")
-          server.close()
-          stop_thread = True
-      else:
-        print(message)
-    except:
-      print("Ocorreu um erro")
-      server.close()
-      break
+    while True:
+        global stop_thread
+        if stop_thread:
+            break
+        try:
+            message = server.recv(2048).decode('ascii')
+            if message == 'NICK':
+                server.send(nickname.encode('ascii'))
+                next_message = server.recv(2048).decode('ascii')
+                if next_message == 'PASS':
+                    server.send(password.encode('ascii'))
+                    if server.recv(2048).decode('ascii') == 'REFUSE':
+                        print("Conexao foi recusada. Senha errada")
+                        stop_thread = True
+                elif next_message == 'BAN':
+                    print("Conexao recusada por causa de ban")
+                    server.close()
+                    stop_thread = True
+            if message == 'QUIT':
+                print("fui mandado embora")
+                stop_thread = True
+            else:
+                print(message)
+        except:
+            print("Ocorreu um erro")
+            server.close()
+            break
+
 
 """funcao que ira escrever mandar a mensagem e nick do cliente
 para o servidor, onde sera feito o broadcast para os demais clientes"""
-def write():
-  while True:
-    if stop_thread == True:
-      break
-    message = f'<{nickname}> {input("")}'
-    if message[len(nickname)+3:].startswith('/'):
-      if nickname == 'admin':
-        if message[len(nickname)+3:].startswith('/kick'):
-          server.send(f'KICK {message[len(nickname)+3+6:]}'.encode('ascii'))
-        if message[len(nickname)+3:].startswith('/ban'):
-          server.send(f'BAN {message[len(nickname)+3+5:]}'.encode('ascii'))
 
-      else:
-        print("Comandos reservados apenas para admin")
-    else:
-      server.send(message.encode('ascii'))
+
+def write():
+    admin_commands = ['/kick', '/ban']
+    while True:
+        global stop_thread
+        if stop_thread == True:
+            break
+        message = f'<{nickname}> {input("")}'
+        if message[len(nickname)+3:].startswith('/'):
+            cmd = message[message.index('/'):]
+            command = ""
+            for c in cmd:
+                if c == ' ':
+                    break
+                command += c
+            if nickname == 'admin':
+                if command == '/kick':
+                    server.send(
+                        f'KICK {message[len(nickname)+3+6:]}'.encode('ascii'))
+                if command == '/ban':
+                    server.send(
+                        f'BAN {message[len(nickname)+3+5:]}'.encode('ascii'))
+            elif command in admin_commands:
+                print("Comandos reservados apenas para admin")
+            elif command == '/quit':
+                server.send(f'QUIT {nickname}'.encode('ascii'))
+                print("pedi para sair")
+                stop_thread = True
+        else:
+            server.send(message.encode('ascii'))
+
 
 """Definicao das threads que vão o tempo todo receber e escrever
 para o server"""
